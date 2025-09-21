41ad52e2d9003691c55c317b616bbffc49fe6171
Muhammad Karimov <karimov.shermukhammad.tech@gmail.com>
2025-09-21 11:57:43 +0500

    updated TrainerStorage.java for explicit imports rather than * wildcards

--- src/main/java/storage/TrainerStorage.java
+++ src/main/java/storage/TrainerStorage.java
@@ -12,7 +12,10 @@ import utility.PasswordGenerator;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * TrainerStorage is a component responsible for
