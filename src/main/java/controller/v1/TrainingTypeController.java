158b0bc264944fade32ba1ffc63aa303f7a5a392
Muhammad Karimov <karimov.shermukhammad.tech@gmail.com>
2025-10-25 18:19:43 +0500

    add in-line logging for training type controller

--- src/main/java/controller/v1/TrainingTypeController.java
+++ src/main/java/controller/v1/TrainingTypeController.java
@@ -2,6 +2,7 @@ package controller.v1;
 
 import dto.response.TrainingTypeResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,7 @@ import service.TrainingTypeService;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/api/v1/training-type", produces = "application/json")
 @RequiredArgsConstructor
@@ -18,7 +20,9 @@ public class TrainingTypeController {
 
     @GetMapping("/")
     public ResponseEntity<List<TrainingTypeResponse>> getTrainingTypes() {
+        log.info("Fetching all training types...");
         List<TrainingTypeResponse> response = trainingTypeService.getAllTrainingTypes();
+        log.info("Fetched {} training types successfully.", response.size());
         return ResponseEntity.ok(response);
     }
 }
