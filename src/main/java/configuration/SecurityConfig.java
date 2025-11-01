014d7c9fb86c74dbcf7495f3671af4cb9eaba5ed
Muhammad Karimov <karimov.shermukhammad.tech@gmail.com>
2025-11-01 16:58:25 +0500

    add actuator and profiles

--- src/main/java/configuration/SecurityConfig.java
+++ src/main/java/configuration/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/swagger-ui/index.html")
+                                "/swagger-ui/index.html",
+                                "/actuator/**")
                         .permitAll()
                         .anyRequest()
                         .authenticated()
